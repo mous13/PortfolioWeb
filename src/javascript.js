37300a116f371922adee8c34a6a8fc1f8bebb200
Oscar-Laptop <oscarx1be@gmail.com>
2023-03-10 20:57:02 +0000

    Loading animation finished with basic web layout completed.

--- src/javascript.js
+++ src/javascript.js
@@ -2,11 +2,17 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
 async function init() {
     const portLoad = document.getElementById("portLoad");
+    const portLoadChildren = document.getElementById("portLoad").children;
     const portContainer = document.getElementById("portContainer");
-    await delay(5000);
-    portLoad.classList.add("fadeout");
-    await delay(1000);
-    // portContainer.classList.add("fadein");
+     await delay(5000);
+     portLoadChildren.item(0).classList.add("slide-out-elliptic-top-bck")
+    await delay(250);
+    portLoadChildren.item(1).classList.add("slide-out-elliptic-top-bck")
+    await delay(250);
+     portLoadChildren.item(2).classList.add("slide-out-elliptic-top-bck")
+
+     await delay(1000);
+    portContainer.classList.add("fadein");
 
 
 
